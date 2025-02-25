9c7e6610415641b7aec1976a1e65f7c6184a0c95
long-kr <longkr.work@gmail.com>
2025-02-24 23:31:17 -0600

    add prettier and config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import HomeLayout from "@/components/layout/HomeLayout";
 export default function Home() {
   return (
     <HomeLayout>
-      <h1>Home</h1>
+      <div className="col-span-1">
+        <div className="container h-screen">Hello</div>
+      </div>
+      <div className="col-span-2">
+        <div className="bg-black h-screen">World</div>
+      </div>
     </HomeLayout>
   );
 }
--- src/components/layout/HomeLayout.tsx
+++ src/components/layout/HomeLayout.tsx
@@ -1,7 +1,10 @@
-
-const HomeLayout = ({ children }: {children: React.ReactNode}) => {
-	//TODO: setup prettier
-	return <div>{children}</div>;
+const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+  //TODO: setup prettier
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-4 grid-flow-col bg-[#DDDDDD]">
+      {children}
+    </div>
+  );
 };
 
 export default HomeLayout;
